58f03cc6b12d1f1a7e1f21f10d72da72e25d31fa
Lígia Andrade <andradeligiamp@gmail.com>
2024-06-15 22:07:08 -0300

    construção skeleton LoadCardA

--- Frontend/src/loaders/LoadCardA/index.tsx
+++ Frontend/src/loaders/LoadCardA/index.tsx
@@ -4,7 +4,22 @@ const LoadCardA = () => {
     return (
         <S.Container className="card">
             <div className="pulsate cardA-title"></div>
-            <div className="pulsate cardA-subtitle"></div>
+            <div className="pulsate cardA-subtitle"></div>         
+            <div className='loadList'>               
+                    <div className='pulsate loadList-Icon'></div>
+                    <div className='pulsate loadList-temperature'></div>
+                    <div className='loadListCondition'>
+                        <div className='pulsate  loadListCondition-title'></div>
+                        <div className='pulsate loadListCondition-subTitle'></div>
+                    </div>            
+            </div>
+            <div className='pulsate CardA-information'></div>
+            <div className='loadConditions'>
+                <div className='pulsate loadConditions-condition'></div>
+                <div className='pulsate loadConditions-button'></div>
+            </div>
+            
+
         </S.Container>
     )
 }
--- Frontend/src/loaders/LoadCardA/styles.ts
+++ Frontend/src/loaders/LoadCardA/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.div`
     width: 786px;
     height: 424px;
     padding: 24px 32px;
+    
 
     .cardA-title {
         width: 130px;
@@ -15,4 +16,65 @@ export const Container = styled.div`
         height: 24px;
         margin-top: 6px;
     }
+    
+    .loadList {
+        display: flex;
+    
+
+        .loadList-Icon{
+            width: 123px;
+            height: 101px;
+            margin-top: 70px;
+        }
+
+        .loadList-temperature{
+            width: 150px;
+            height: 88px;
+            margin-top: 85px;
+            margin-left: 10px;
+        }
+
+        .loadListCondition{
+            margin-left: 25px;
+            
+            .loadListCondition-title{
+                width: 318px;
+                height: 30px;
+                margin-top: 100px;            
+            }
+
+            .loadListCondition-subTitle{
+                width: 228px;
+                height: 27px;
+                margin-top: 10px;                
+            }
+        }
+    }
+
+    .CardA-information{
+        width: 286px;
+        height: 25px;
+        margin-top: 7px;
+    }
+
+    .loadConditions{
+        display: flex;
+        margin-top: 75px;
+
+        .loadConditions-condition{
+        width: 480px;
+        height: 40px;
+        
+    }
+
+        .loadConditions-button{
+            width: 207px;
+            height: 40px;
+            margin-left: 15px;
+            border-radius: 27px;
+        }
+
+    }
+
+    
 `
